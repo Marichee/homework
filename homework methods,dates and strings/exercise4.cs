71f3a7f4ab846dcdce852d553a9e0a9927296ede
Marichee <maricheprosheva@gmail.com>
2019-03-09 19:57:37 +0100

    methods,dates and strings

---
+++ homework methods,dates and strings/exercise4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5_date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Today;
+            Console.WriteLine("Today is: " + dateTime);
+            Console.WriteLine("Three days from now: "+dateTime.AddDays(3));
+            Console.WriteLine("One month from now: " + dateTime.AddMonths(1));
+            Console.WriteLine("One month and three days from now: " + dateTime.AddMonths(1).AddDays(3));
+            Console.WriteLine("One year and two months ago from now: " + dateTime.AddYears(-1).AddMonths(-2));
+            Console.WriteLine("Current month with words: "+dateTime.ToString("MMMM"));
+            Console.WriteLine("Current year: "+dateTime.Year);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework methods,dates and strings/exercise5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5_age_calculatoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birthday date in format DD-MM-YYYY");
+            string birthDay = Console.ReadLine();
+            DateTime date = Convert.ToDateTime(birthDay);
+            CalculateAge(date);
+            Console.ReadLine();
+        }
+
+        public static int CalculateAge(DateTime birthDay)
+        {
+            int years = DateTime.Now.Year - birthDay.Year;
+            if ((birthDay.Month > DateTime.Now.Month) || (birthDay.Month == DateTime.Now.Month && birthDay.Day > DateTime.Now.Day))
+            { years--; }
+            Console.WriteLine("You are: " + years);
+            return years;
+        }
+
+
+    }
+}
