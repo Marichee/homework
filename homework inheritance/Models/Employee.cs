8b7b2e2b32f0d57123e4636fa98e0bd53f47d54a
Marichee <maricheprosheva@gmail.com>
2019-03-25 23:17:24 +0100

    homework inheritance

---
+++ homework inheritance/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace inheritance_library
+{
+
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public Role Role { get; set; }
+        protected double Salary { get; set; }
+        public Employee(string firstname, string lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+        public string PrintInfo()
+        {
+            return $"{Firstname} {Lastname} {Salary}";
+        }
+        public virtual double GetSalary(double salary)
+        {
+            return Salary = salary;
+        }
+    }
+}
