79352627747ed12cdb584723b51d83823998b981
Marichee <maricheprosheva@gmail.com>
2019-04-03 01:33:23 +0200

    homework Generic collections

---
+++ Generic collections/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generetic_collections.Models
+{
+    public class Person
+    {
+
+        public Person(string firstname, string lastname, int id, int age, Genre genre)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Age = age;
+            FavoriteMusicType = genre;
+        }
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public Genre FavoriteMusicType { get; set; }
+        public List<Song> FavoriteSongs { get; set; }
+
+        public void GetFavSongs()
+        {
+            if (FavoriteSongs.Count == 0)
+            {
+                Console.WriteLine("this person hates music");
+            }
+            else
+            {
+                foreach (Song song in FavoriteSongs)
+                {
+                    Console.WriteLine(song.Title);
+                }
+            }
+        }
+    }
+}
---
+++ Generic collections/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generetic_collections.Models
+{
+   public class Song
+    {
+        public Song(string title, int length, Genre genre)
+        {
+            Title = title;
+            Length = length;
+            Genre = genre;
+        }
+        public string Title { get; set; }
+        public int Length { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
+
