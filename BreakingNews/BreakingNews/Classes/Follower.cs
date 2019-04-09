b80058e0a63f6a5ca69635452c7949f68cbceb93
Marichee <maricheprosheva@gmail.com>
2019-04-09 10:06:37 +0200

    homework delegates

---
+++ BreakingNews/BreakingNews/Classes/Follower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingNews.Classes
+{
+    public class Follower
+    {
+        public string Name { get; set; }
+        public Follower(string name)
+        {
+            Name = name;
+        }
+        public void Subscribe(Medium medium)
+        {
+            medium.News += new Medium.BreakingNews(News);
+        }
+        public void News(Medium medium)
+        {
+            Console.WriteLine($"Follower : {this.Name}\n News :\n {medium.NewsOne}\n{medium.NewsTwo}\n Date: {medium.DateTime}\n");
+        }
+    }
+}
---
+++ BreakingNews/BreakingNews/Classes/Medium.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingNews.Classes
+{
+    public class Medium
+    {
+        public string NewsOne { get; set; }
+        public string NewsTwo { get; set; }
+        public DateTime DateTime { get; set; }
+        public delegate void BreakingNews(Medium medium);
+        public event BreakingNews News;
+        public void SendBreakingNews(string newsOne, string newsTwo, DateTime dateTime)
+        {
+            NewsOne = newsOne;
+            NewsTwo = newsTwo;
+            DateTime = dateTime;
+            if (News != null)
+            {
+                News(this);
+            }
+        }
+    }
+}
